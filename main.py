b374ebf800590879e95ec2286282fbc3ac01be7a
Aliya Mullina <aliya.mullina@ya.ru>
2021-07-27 14:44:36 +0300

    Переименование переменных, заготовка двух функций: подготовка к записи в бд, запись в бд

--- main.py
+++ main.py
@@ -13,21 +13,40 @@ def load_json(file_json: str):
         return False
 
 
-def validate_schema(add_json: dict, add_schema: dict):
+def validate_json_schema(file_json: dict, file_schema: dict):
     """ Происходит валидация входных данных."""
     try:
-        jsonschema.validate(add_json, add_schema)
+        jsonschema.validate(file_json, file_schema)
         return True
     except jsonschema.exceptions.ValidationError as message:
         print('Ошибка валидации json:', message)
         return False
 
 
+def prepare_json_to_db(file_json: dict):
+    """ Подготовка к сохранению в базу в две таблицы. """
+    table_goods = []
+    table_shops_goods = []
+
+    return table_goods, table_shops_goods
+
+
+def add_json_to_db(table_goods, table_shops_goods):
+    """ Сохранение в базу в две таблицы. """
+    # Приложение создаёт таблицы если они не созданы.
+    # Приложение только вставляет данные, но не делает удаления.
+    # Если пришли новые данные по предмету уже имеющемуся в базе — обновить.
+    # Использовать либо sqlite3 либо postgre для хранения данных.
+
+
 def main():
     """ Старт программы. """
-    add_json = load_json('goods.file.json')
-    add_schema = load_json('goods.schema.json')
-    validate_schema(add_json, add_schema)
+    file_json = load_json('goods.file.json')
+    file_schema = load_json('goods.schema.json')
+
+    if validate_json_schema(file_json, file_schema):
+        table_goods, table_shops_goods = prepare_json_to_db(file_json)
+        add_json_to_db(table_goods, table_shops_goods)
 
 
 main()
