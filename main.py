320d9ed41bc303c4cf37996f06d628549164c645
Aliya Mullina <aliya.mullina@ya.ru>
2021-07-27 12:39:30 +0300

    Загрузка json файла

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import json
 
+
+def load_json(file_json: str):
+    """ В качестве входных параметров программа получает файл json. """
+    try:
+        with open(file_json, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            return data
+    except Exception as message:
+        print('Ошибка загрузки json:', message)
+        return False
+
+
+input_json = load_json('goods.schema.json')
